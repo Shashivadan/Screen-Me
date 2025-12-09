fa6b55dec001367d341d59fe4cc191c55c5ca2c3
shashivadan <shashivadan99@gmail.com>
2025-12-09 23:53:41 +0530

    feat: app optmiztion

--- apps/www/src/routes/index.tsx
+++ apps/www/src/routes/index.tsx
@@ -1,12 +1,5 @@
 import HeroSection from '@/components/hero-section'
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/')({ component: App })
+export const Route = createFileRoute('/')({ component: HeroSection })
 
-function App() {
-  return (
-    <div className="">
-      <HeroSection />
-    </div>
-  )
-}
